7de73ef3b52d4875df35fdfa30a7506c016c7bda
xinkent <xinkent@gmail.com>
2018-04-05 09:20:18 +0900

    update

--- train.py
+++ train.py
@@ -10,26 +10,9 @@ from load_dataset import *
 from model import FullyConvolutionalNetwork
 import warnings
 import argparse
+from color_map import make_color_map
 # warnings.filterwarnings('ignore')
 
-def make_color_map():
-    n = 256
-    cmap = np.zeros((n, 3)).astype(np.int32)
-    for i in range(0, n):
-        d = i - 1
-        r,g,b = 0,0,0
-        for j in range(0, 7):
-            r = bitor(r, shift_bit(get_bit(d, 0), 7 - j))
-            g = bitor(g, shift_bit(get_bit(d, 1), 7 - j))
-            b = bitor(b, shift_bit(get_bit(d, 2), 7 - j))
-            d = shift_bit(d, -3)
-        cmap[i, 0] = b
-        cmap[i, 1] = g
-        cmap[i, 2] = r
-    return cmap[1:22]
-
-
-np.random.seed(seed=32)
 
 def train():
     parser = argparse.ArgumentParser()
@@ -54,6 +37,7 @@ def train():
     f.write('epoch:' + str(args.epoch) + '\n' + 'batch:' + str(batchsize) + '\n' + 'lr:' + str(lr))
     f.close()
 
+    np.random.seed(seed=32)
     img_size = 512
     nb_classes = 5
     names = os.listdir(path_to_train)
