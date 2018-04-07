825c433e88bb96b993d7bebf0ae74b318411f90e
xinkent <xinkent1994@gmail.com>
2018-04-07 21:09:17 +0900

    update

--- train.py
+++ train.py
@@ -75,7 +75,7 @@ def train():
     train_model.compile(loss=crossentropy, optimizer=adam)
     # train_model.fit_generator(generate_arrays_from_file(train_names, path_to_train, path_to_target, img_size, nb_class),
     #                                                steps_per_epoch=nb_data/1, epochs=1000)
-    train_model.fit(train_X,train_y,batch_size = batchsize, epochs=epoch)
+    train_model.fit(train_X,train_y,batch_size = batchsize, epochs=epoch, validation_split=0.2)
     train_model.save_weights(out + '/weights.h5')
 
 
